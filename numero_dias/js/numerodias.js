563ae22731362064ced6fb36e575c1380e09325d
Javi <jruiztoledog@gmail.com>
2015-10-16 13:45:59 +0200

    cambiado

--- numero_dias/js/numerodias.js
+++ numero_dias/js/numerodias.js
@@ -78,61 +78,46 @@ function calculateDaysSinceEpoch(date) {
         } else {
             while ((firstYear <= year) && (firstMonth <= monthNumber) && (firstDay < day)) {
                 
+                if (firstYear === year && firstMonth === monthNumber) {
+                    contDays += day - firstDay;
+                } else {
+                    switch (firstMonth) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        maxdays = 31;
 
-                switch (firstMonth) {
-                case 1:
-                    maxdays = 31;
-                    break;
-                case 2:
-                    if (firstYear % 4 && (firstYear % 400 || !(firstYear % 100))) {
-                        maxdays = 28;
-                    } else {
-                        maxdays = 29;
-                    }
-                    break;
-                case 3:
-                    maxdays = 31;
-                    break;
-                case 4:
-                    maxdays = 30;
-                    break;
-                case 5:
-                    maxdays = 31;
-                    break;
-                case 6:
-                    maxdays = 30;
-                    break;
-                case 7:
-                    maxdays = 31;
-                    break;
-                case 8:
-                    maxdays = 31;
-                    break;
-                case 9:
-                    maxdays = 30;
-                    break;
-                case 10:
-                    maxdays = 31;
-                    break;
-                case 11:
-                    maxdays = 30;
-                    break;
-                case 12:
-                    maxdays = 31;
-                    break;
-                default:
-                    monthNumber = 0;
-                    break;
-                }
-                contDays += maxdays;
-                firstMonth += 1;
-                while (firstMonth === 13) {
+                    case 2:
+                        if (firstYear % 4 && (firstYear % 400 || !(firstYear % 100))) {
+                            maxdays = 28;
+                        } else {
+                            maxdays = 29;
+                        }
+                        break;
+                    
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        maxdays = 30;
+                        break;
+
+                    default:
+                        monthNumber = 0;
+                        break;
+                    } 
+                    contDays += maxdays;
+                    firstMonth += 1;
+                    while (firstMonth === 13) {
                     firstYear += 1;
                     firstMonth = 1;
+                    }
                 }
-                while (firstYear === year && firstMonth === monthNumber) {
-                    contDays += day - firstDay;
-                }
+                
             }
             output = contDays;
         }
