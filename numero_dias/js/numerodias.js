dd9eddf26ee2a8e2a55bc3b27ff9794764605d05
Javi <jruiztoledog@gmail.com>
2015-10-24 17:27:45 +0200

    fechas

--- numero_dias/js/numerodias.js
+++ numero_dias/js/numerodias.js
@@ -1,19 +1,20 @@
 function calculateDaysSinceEpoch(date) {
     var contDays = 0,
         day = (date[0] + date[1]) * 1,
+        validDay =  (!(day < 1 || day > 31) && !(date[0] === " " || date[1] === " ")),
         monthLetters = date[2] + date[3] + date[4],
         year = 1 * (date[5] + date[6] + date[7] + date[8]),
+        validYear = (!(year <= 0 || year > 9999) && !(date[9] + 1) && !isNaN(year)),
         monthNumber,
         maxdays,
         output,
         bis = (!(year % 4) && ((year % 100) || !(year % 400))),
-        FIRSTDAY = 1,
+        i,
         FIRSTMONTH = 1,
         FIRSTYEAR = 1970,
         FORMATO = "Por favor, introducir la fecha en el formato solicitado",
         NOVALIDA = "La fecha introducida no es válida";
-
-        switch (monthLetters) {
+    switch (monthLetters) {
     case "jan":
         monthNumber = 1;
         maxdays = 31;
@@ -26,7 +27,7 @@ function calculateDaysSinceEpoch(date) {
         monthNumber = 3;
         maxdays = 31;
         break;
-    case "abr":
+    case "apr":
         monthNumber = 4;
         maxdays = 30;
         break;
@@ -66,95 +67,46 @@ function calculateDaysSinceEpoch(date) {
         monthNumber = 0;
         break;
     }
-
-    if ((monthNumber === 0) || (year <= 0) || (day < 1 || day > 31)) {
+    if (monthNumber === 0 || !validDay || !validYear) {
         output = FORMATO;
     } else {
-        if ((year < 1970) || (day > maxdays)) {
+        if (year < 1970) {
+            output = NOVALIDA;
+        } else if (day > maxdays) {
             output = NOVALIDA;
         } else {
-            debugger;
-            if (FIRSTYEAR === year && FIRSTMONTH === monthNumber) {
-                contDays += day - FIRSTDAY;
-            } else {
-                while ((FIRSTYEAR <= year) && (FIRSTMONTH < monthNumber)) {
-                debugger;
-                    switch (FIRSTMONTH) {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 8:
-                    case 10:
-                    case 12:
-                        maxdays = 31;
-                        break;
-
-                    case 2:
-                        maxdays = bis ? 29 : 28;
-                        break;
-
-                    case 4:
-                    case 6:
-                    case 9:
-                    case 11:
-                        maxdays = 30;
-                        break;
-
-                    default:
-                        monthNumber = 0;
-                        break;
-                    }
-                    contDays += maxdays;
-
-                    if ((FIRSTMONTH + 1) === 13) {
-                        FIRSTYEAR += 1;
-                        FIRSTMONTH = 1;
-                    } else {
-                        FIRSTMONTH += 1;
-                    }
-                    
-                    while ((FIRSTYEAR <= year) || (FIRSTMONTH < monthNumber)) {
-                        switch (FIRSTMONTH) {
-                        case 1:
-                        case 3:
-                        case 5:
-                        case 7:
-                        case 8:
-                        case 10:
-                        case 12:
-                            maxdays = 31;
-                            break;
-
-                        case 2:
-                            maxdays = bis ? 29 : 28;
-                            break;
-
-                        case 4:
-                        case 6:
-                        case 9:
-                        case 11:
-                            maxdays = 30;
-                            break;
-
-                        default:
-                            monthNumber = 0;
-                            break;
-                        }
-
-                        contDays += maxdays - FIRSTDAY;
-
-                        if ((FIRSTMONTH + 1) === 13) {
-                            FIRSTYEAR += 1;
-                            FIRSTMONTH = 1;
-                        } else {
-                            FIRSTMONTH += 1;
-                        }
-                    }
-                    contDays += day;
-                }
+            for (i = FIRSTYEAR; i < year; i += 1) {
+                contDays += (!(i % 4) && ((i % 100) || !(i % 400))) ? 366 : 365;
             }
-            output = contDays;
+            while (FIRSTMONTH < monthNumber) {
+                switch (FIRSTMONTH) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    maxdays = 31;
+                    break;
+                case 2:
+                    maxdays = bis ? 29 : 28;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    maxdays = 30;
+                    break;
+                default:
+                    monthNumber = 0;
+                    break;
+                }
+                contDays += maxdays;
+                FIRSTMONTH += 1;
+            }
+            contDays += day;
+            output = contDays - 1;
         }
     }
     return output;
