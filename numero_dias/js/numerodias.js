f39ad46a1eb8f8adb923e849fc0b26c5013e3fcd
Javi <jruiztoledog@gmail.com>
2015-10-24 17:48:19 +0200

    Fecha Unix

--- numero_dias/js/numerodias.js
+++ numero_dias/js/numerodias.js
@@ -1,5 +1,5 @@
 function calculateDaysSinceEpoch(date) {
-    var contDays = 0,
+    var countDays = 0,
         day = (date[0] + date[1]) * 1,
         validDay =  (!(day < 1 || day > 31) && !(date[0] === " " || date[1] === " ")),
         monthLetters = date[2] + date[3] + date[4],
@@ -8,12 +8,12 @@ function calculateDaysSinceEpoch(date) {
         monthNumber,
         maxdays,
         output,
-        bis = (!(year % 4) && ((year % 100) || !(year % 400))),
+        leap = (!(year % 4) && ((year % 100) || !(year % 400))),
         i,
         FIRSTMONTH = 1,
         FIRSTYEAR = 1970,
-        FORMATO = "Por favor, introducir la fecha en el formato solicitado",
-        NOVALIDA = "La fecha introducida no es válida";
+        FORMAT_ERROR = "Por favor, introducir la fecha en el formato solicitado",
+        NOT_VALID = "La fecha introducida no es válida";
     switch (monthLetters) {
     case "jan":
         monthNumber = 1;
@@ -21,7 +21,7 @@ function calculateDaysSinceEpoch(date) {
         break;
     case "feb":
         monthNumber = 2;
-        maxdays = bis ? 29 : 28;
+        maxdays = leap ? 29 : 28;
         break;
     case "mar":
         monthNumber = 3;
@@ -68,17 +68,17 @@ function calculateDaysSinceEpoch(date) {
         break;
     }
     if (monthNumber === 0 || !validDay || !validYear) {
-        output = FORMATO;
+        output = FORMAT_ERROR;
     } else {
         if (year < 1970) {
-            output = NOVALIDA;
+            output = NOT_VALID;
         } else if (day > maxdays) {
-            output = NOVALIDA;
+            output = NOT_VALID;
         } else {
-            for (i = FIRSTYEAR; i < year; i += 1) {
-                contDays += (!(i % 4) && ((i % 100) || !(i % 400))) ? 366 : 365;
+            for (i = FIRSTYEAR; i < year; i += 1) {//<====== Add days from 1970 to actual year (full years)
+                countDays += (!(i % 4) && ((i % 100) || !(i % 400))) ? 366 : 365;
             }
-            while (FIRSTMONTH < monthNumber) {
+            while (FIRSTMONTH < monthNumber) {// <===== Add days from january to actual month (full months)
                 switch (FIRSTMONTH) {
                 case 1:
                 case 3:
@@ -90,7 +90,7 @@ function calculateDaysSinceEpoch(date) {
                     maxdays = 31;
                     break;
                 case 2:
-                    maxdays = bis ? 29 : 28;
+                    maxdays = leap ? 29 : 28;
                     break;
                 case 4:
                 case 6:
@@ -102,11 +102,11 @@ function calculateDaysSinceEpoch(date) {
                     monthNumber = 0;
                     break;
                 }
-                contDays += maxdays;
+                countDays += maxdays;// <===== Days from this month not counted yet
                 FIRSTMONTH += 1;
             }
-            contDays += day;
-            output = contDays - 1;
+            countDays += day;
+            output = countDays - 1;// <==== Substract 1 day from "1-jan-1970"
         }
     }
     return output;
