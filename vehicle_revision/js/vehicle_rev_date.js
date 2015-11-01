318a36bcfce64672f98995083a220d01010578db
jrtgad <jruiztoledog@gmail.com>
2015-11-01 23:45:01 +0100

    js

--- vehicle_revision/js/vehicle_rev_date.js
+++ vehicle_revision/js/vehicle_rev_date.js
@@ -1,29 +1,91 @@
-function showVehicleRevisionStatus (string) {
-    var template = new RegExp("^[0-9]{4}[- ]?[A-Z]{3}$|
-                               ^[A-Z]{1,2}-?[0-9]{4}-?[A-Z]{1,2}$",
-                               "i"),
-
-                    /*/^[0-9]{4}-?[A-Z]{3}$/  También vale*/
-
-    //Recibe ?user=string&mat=string&rev=string
-        numberplate = ,
-        lastrevdate = ,
+function showVehicleRevisionStatus(string) {
+    //Recibe ?mat=string&rev=string
+  var vars = getVars(string),
+        numberplate = vars[0],
+        lastrevdate = vars[1],
         today = new Date(),
-        day = today.getUTCDate(),
-        month = today.getUTCMonth() + 1,
-        year = today.getUTCFullYear(),
+        Actualday = today.getUTCDate(),
+        Actualmonth = today.getUTCMonth() + 1,
+        Actualyear = today.getUTCFullYear(),
         output;
 
-    if(!template.test(mat)) {
-        output = ERR_MAT;
-    } else if(!checkDates()) {
-
-    }
-
+            !validatePlate(numberplate) ? output = ERR_MAT :
+            !checkDates(lastrevdate) ? output = ERR_MAT :
 
     return output;
 }
 
-/*function checkDate(date) {
+function validatePlate(mat) {
+  var template = new RegExp("^[0-9]{4}[- ]?[A-Z]{3}$| ^[A-Z]{1,2}-?[0-9]{4}-?[A-Z]{1,2}$",
+                             "i");
+   /*/^[0-9]{4}-?[A-Z]{3}$/  También vale*/
+  return template.test(mat);
+}
 
-}*/
+function getVars(stringVars) {
+  var value1 = stringVars.subString(stringVars.indexOf("mat=") + 4, stringVars.indexOf("&")),
+      value2 = stringVars.subString(stringVars.indexOf("rev=") + 4, stringVars.length);
+
+  return [value1, value2];
+}
+
+function checkDate(date) {
+
+}
+
+function monthStringToNumber(month) {
+  switch (monthLetters) {
+    case "jan":
+        monthNumber = 1;
+        maxdays = 31;
+        break;
+    case "feb":
+        monthNumber = 2;
+        maxdays = leap ? 29 : 28;
+        break;
+    case "mar":
+        monthNumber = 3;
+        maxdays = 31;
+        break;
+    case "apr":
+        monthNumber = 4;
+        maxdays = 30;
+        break;
+    case "may":
+        monthNumber = 5;
+        maxdays = 31;
+        break;
+    case "jun":
+        monthNumber = 6;
+        maxdays = 30;
+        break;
+    case "jul":
+        monthNumber = 7;
+        maxdays = 31;
+        break;
+    case "aug":
+        monthNumber = 8;
+        maxdays = 31;
+        break;
+    case "sep":
+        monthNumber = 9;
+        maxdays = 30;
+        break;
+    case "oct":
+        monthNumber = 10;
+        maxdays = 31;
+        break;
+    case "nov":
+        monthNumber = 11;
+        maxdays = 30;
+        break;
+    case "dec":
+        monthNumber = 12;
+        maxdays = 31;
+        break;
+    default:
+        monthNumber = 0;
+        break;
+    }
+    return [monthNumber, maxdays];
+}
