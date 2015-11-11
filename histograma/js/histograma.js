542259c17020401be4df5e544cc2fdfcb7fa3aa5
jrtgad <jruiztoledog@gmail.com>
2015-11-11 21:18:45 +0100

    practica

---
+++ histograma/js/histograma.js
@@ -0,0 +1,18 @@
+function cuentaCaracteres(queryString) {
+	var i,
+		resultado,
+		caracteresArray = [],
+		queryString = queryString.toUpperCase().replace(" ", "_");
+
+	for (i = 0; i < queryString.length; i += 1) {
+		if (caracteresArray.indexOf(queryString[i]) !== -1) {
+			caracteresArray[caracteresArray.indexOf(queryString[i]) + 1] += 1;
+		} else {
+			caracteresArray.push(queryString[i], 0);
+		}
+		
+	}
+	resultado = creaHistograma(caracteresArray);
+
+	return resultado;
+}
