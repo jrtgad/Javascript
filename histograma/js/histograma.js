15a81a3870c4812d9c80580c5c60e0ce25fc383c
jrtgad <jruiztoledog@gmail.com>
2015-11-26 01:59:08 +0100

    n

--- histograma/js/histograma.js
+++ histograma/js/histograma.js
@@ -1,16 +1,18 @@
 function cuentaCaracteres(queryString) {
-    var i,
-        resultado,
-        caracteresArray = [],
-        queryString = queryString.toUpperCase();
+	var i,
+		resultado,
+		caracteresArray = {},
+		queryString = queryString.toUpperCase().replace(" ", "_");
 
-    for (i = 0; i < queryString.length; i += 1) {
-        if (caracteresArray.indexOf(queryString[i]) !== -1) {
-            caracteresArray[caracteresArray.indexOf(queryString[i]) + 1] += "*";
-        } else {
-            caracteresArray.push(queryString[i], "*");
-        }
-        
-    }
-    return caracteresArray;
+	for (i = 0; i < queryString.length; i += 1) {
+		if (caracteresArray.indexOf(queryString[i]) !== -1) {
+			caracteresArray[caracteresArray.indexOf(queryString[i]) + 1] += 1;
+		} else {
+			caracteresArray.push(queryString[i], 0);
+		}
+		
+	}
+	resultado = creaHistograma(caracteresArray);
+
+	return resultado;
 }
